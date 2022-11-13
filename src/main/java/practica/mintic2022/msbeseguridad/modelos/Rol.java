4719d526d26f6bc07281d2807e1242d33a688aa3
pipe <pipe@gmail.com>
2022-11-13 12:35:15 -0500

    crud completo a tabla rol

--- src/main/java/practica/mintic2022/msbeseguridad/modelos/Rol.java
+++ src/main/java/practica/mintic2022/msbeseguridad/modelos/Rol.java
@@ -1,5 +1,20 @@
 package practica.mintic2022.msbeseguridad.modelos;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+import lombok.Setter;
+import lombok.AccessLevel;
+
+@Document
+@Data
 public class Rol {
-    
+    @Id
+    @Setter(AccessLevel.NONE) //le establezco acces none para qeu no pued amodificarse el id
+    private String _id;
+
+    private String descripcion;
+    private String tipo;
+
 }
